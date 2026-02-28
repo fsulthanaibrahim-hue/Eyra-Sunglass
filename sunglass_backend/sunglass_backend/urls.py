20623bd12a39d1bee943e02c537597a696154327
fsulthanaibrahim-hue <fsulthanaibrahim@gmail.com>
2026-02-28 12:48:33 -0800

    Eyra-Sunglass

--- sunglass_backend/sunglass_backend/urls.py
+++ sunglass_backend/sunglass_backend/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Eyra Sunglass Backend is Live 🚀")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/products/', include('products.urls')),
