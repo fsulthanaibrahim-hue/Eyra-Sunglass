d53839d9147b144d4670773665f4b8f40ffd9765
fsulthanaibrahim-hue <fsulthanaibrahim@gmail.com>
2026-02-28 14:12:29 -0800

    Eyra-Sunglass

--- sunglass_backend/sunglass_backend/settings.py
+++ sunglass_backend/sunglass_backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6rub7hyb*-feb9o7@(sbu9@_bwxa5au5*sat_+*vfd7^8td=b5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['eyra-sunglass.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
