4c25610b3bb63a984969d1c617fa291058779050
fsulthanaibrahim-hue <fsulthanaibrahim@gmail.com>
2026-09-11 21:57:17 +0530

    update home page

--- sunglass_frontend/src/pages/Home.jsx
+++ sunglass_frontend/src/pages/Home.jsx
@@ -331,7 +331,7 @@ export default function Home() {
         }
         .section-divider {
           width: 50px;
-          height: 1px;
+          height: 2px;
           background: #C9974A;
           margin: 16px auto 0;
         }
