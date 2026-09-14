deab583ab3b6dd67af17b99290d6b2520af5fab6
fsulthanaibrahim-hue <fsulthanaibrahim@gmail.com>
2026-09-14 08:53:30 +0530

    Update Docker Production setup

--- sunglass_backend/sunglass_backend/settings.py
+++ sunglass_backend/sunglass_backend/settings.py
@@ -15,6 +15,8 @@ import os
 from decouple import config
 import dj_database_url
 from datetime import timedelta
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +30,13 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com').split(',')
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in config(
+        'ALLOWED_HOSTS',
+        default='localhost,127.0.0.1,.onrender.com'
+    ).split(',')
+]
 
 
 # Application definition
@@ -95,7 +103,7 @@ DATABASES = {
         'USER': config('POSTGRES_USER'),
         'PASSWORD': config('POSTGRES_PASSWORD'),
         'HOST': config('POSTGRES_HOST'),
-        'PORT': config('POSTGRES_PORT', default='5432'),
+        'PORT': config('POSTGRES_PORT', cast=int),
     }
 }
 
